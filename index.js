f27837261a497bb4744023b2089c3f87575325f9
jjmirwaldjr <jjmirwaldjr@gmail.com>
2022-07-05 13:42:39 -0400

    created the header and dropdown menu

--- index.js
+++ index.js
@@ -1,35 +1,5 @@
-import DonutMaker from './DonutMaker.js'
-
-renderPage();
-
-function renderPage(){
-  donutMaker();
-}
-
-// const container = (document.querySelector('.container').innerText =
-//   'mmmmmmmmm doughnuts.... ahhhhhhh!');
-
-function donutMaker(){
-  const createBtn = document.querySelector('#button');
-  const donutCounter = document.querySelector('#donutCounter');
-  const autoClicker = document.querySelector('#autoClicker');
-  const donutMulti = document.querySelector('#multiplier');
-  
-  
-  const createdCounter = new DonutMaker(donutCounter.value, autoClicker.value,
-      donutMulti.value);
-
-      donutCounter.innerText = createdCounter.donutCounter;
-      autoClicker.innerText = createdCounter.autoClicker;
-      donutMulti.innerText = createdCounter.donutMulti;
-
-  // console.log('this is working!!!!');
-  
-    //Displays Counters
-  // displayDonutsCounters(createdCounter);
-  
-  //constructor for the counters
+const container = (document.querySelector('.container').innerText =
+  'mmmmmmmmm doughnuts.... ahhhhhhh!');
 
 
 
-}
